c5ffc00c4d0972f6ce6dad76be7f3deed78daf48
prashArt3d <ppsawant1985@gmail.com>
2025-07-15 22:13:57 +0530

    added code

--- new_branch.py
+++ new_branch.py
@@ -3,3 +3,17 @@ second_name= "doe"
 
 full_name = first_name + second_name 
 print(full_name)
+
+
+if new_number == 100:
+ print("number is 100")
+
+greeting = "hello"
+if greeting == "hello":
+   print ("greeting is hello ")
+
+tempreture = 10
+if tempreture > 0 :
+   print ("tempreture is above freezing." )
+
+###-----------if-else Statement-----------
